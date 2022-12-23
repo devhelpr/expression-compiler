df7ed214cf92c1f7151c46bc01ba331b3e96f2f8
Maikel van de Lisdonk <maikel@devhelpr.com>
2022-12-23 12:23:23 +0100

    vite config for npm path style with @ scopes

--- expression-compiler/vite.config.ts
+++ expression-compiler/vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points.
       entry: 'src/index.ts',
-      name: 'expression-compiler',
+      name: '@devhelpr/expression-compiler',
       fileName: 'index',
       // Change this to the formats you want to support.
       // Don't forgot to update your package.json as well.
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   server: {
@@ -10,6 +11,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    tsconfigPaths(),
     viteTsConfigPaths({
       root: './',
     }),
