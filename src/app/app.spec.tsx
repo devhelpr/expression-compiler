8e20f04aab1519bbdb6650cee347a81d2dc960f0
Maikel van de Lisdonk <maikel@devhelpr.com>
2023-01-07 08:46:04 +0100

    UT fix

--- src/app/app.spec.tsx
+++ src/app/app.spec.tsx
@@ -2,6 +2,10 @@ import { render } from '@testing-library/react';
 
 import App from './app';
 
+Object.defineProperty(window, 'crypto', {
+  value: { randomUUID: () => '1234' },
+});
+
 describe('App', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<App />);
